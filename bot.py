e65c77cbf1967f34eabccb11bfc00cf9fc721b7f
muhammadyusufpolatov932-max <muhammadyusufpolatov932@gmail.com>
2026-07-15 16:11:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,6 +116,7 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await waiting.edit_text(
             f"❌ Xatolik:\n{e}"
         )
+
 def main():
     if not BOT_TOKEN:
         raise ValueError(
